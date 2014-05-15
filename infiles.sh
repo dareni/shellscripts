5aca301ca11c455c520916ef3eb8fcd7336a0da7
Daren Isaacs <ikkyisaacs@gmail.com>
2014-05-15 14:26:59 +1000

    Adjust infiles.sh

--- infiles.sh
+++ infiles.sh
@@ -1,8 +1,46 @@
 #!/bin/bash
-for f in `find . -name "$1"`; 
+if [[ -z "$1" ]]; then
+    echo Usage: infiles.sh search_pattern filetype 
+    echo filetype war jar zip
+    exit -1;
+fi
+
+SEARCH_PATTERN=$1
+
+if [[ -z "$2" ]]; then
+    FILETYPE=jar
+else
+    FILETYPE=$2
+fi
+
+#    find . -name "*.$FILETYPE" -exec jar -tvf {} \; |grep -H $SEARCH_PATTERN
+#| grep -H -i 
+#    exit 0;;
+
+for f in `find . -name "*.$FILETYPE"`; 
 do  
-    count=`grep -c $2 $f;`
-    if [[ $count -gt 0 ]]; then
-        echo  $f
+
+    case $FILETYPE in
+        war|jar)
+            count=`jar -tvf $f |grep -c -i $SEARCH_PATTERN`;;
+        zip)
+            count=`unzip -l $f |grep -c -i $SEARCH_PATTERN`;;
+        *)
+            count=`grep -c -i $2 $f;`;;
+    esac
+
+    if [[ $count -gt 2 ]]; then
+        echo $f
+        case $FILETYPE in
+            war|jar)
+                jar -tvf $f |grep -i $SEARCH_PATTERN;;
+            zip)
+                unzip -l $f |grep -i $SEARCH_PATTERN;;
+            *)
+                grep -i $SEARCH_PATTERN $f;;
+        esac
     fi
+
+
 done;
+
