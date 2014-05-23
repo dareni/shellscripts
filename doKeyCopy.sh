57867c828bd5b5e43382ec72a108856c6f18f0cb
Daren Isaacs <ikkyisaacs@gmail.com>
2014-05-23 16:23:53 +1000

    doKeyCopy fix

--- doKeyCopy.sh
+++ doKeyCopy.sh
@@ -1,5 +1,6 @@
-if [[ -z "$1" ]]; then
+#!/bin/sh
+if [ -z "$1" ]; then
     echo "Usage: ./doKeyCopy.sh username@host.domain"
 else
-    cat ~/.ssh/id_rsa.pub | ssh $1 'dd >> ~/.ssh/authorized_keys; if [[ ! -d ~/.ssh ]]; then mkdir ~/.ssh; chmod 700 ~/.ssh; echo Just created ~/.ssh please run again; fi'
+    cat ~/.ssh/id_rsa.pub | ssh $1 'dd >> ~/.ssh/authorized_keys; if [ ! -d ~/.ssh ]; then mkdir ~/.ssh; chmod 700 ~/.ssh; echo Just created ~/.ssh please run again; fi'
 fi
