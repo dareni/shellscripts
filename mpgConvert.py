33a3ac697a587cac755a2cb31ad2ed1f602c584d
daren <daren@beastie.localdomain>
2014-06-28 13:28:51 +1000

    Bug fix.

--- mpgConvert.py
+++ mpgConvert.py
@@ -1,11 +1,4 @@
 #!/usr/local/bin/python
-#Frozen [2013]  Soundtrack (Deluxe Edition) (Christophe Beck) YG
-#Bon Jovi The Ultimate Collection 2013 [Remastered]
-#Coldplay - X & Y
-#Elton John The Very Best Of Magic Elton John 2010 [320 Kbps]
-#Kylie Minogue - The Best of Kylie Minogue (2012)
-#01 Titanium (feat. Sia).mp3
-#04 Happy.mp3
 
 import os
 from sys import argv
@@ -38,11 +31,10 @@ if not os.path.isdir(outdir):
 for targetfile in files:
     filename = os.path.basename(targetfile)
     name, extension = os.path.splitext(filename)
-    cmd = 'avconv  -i "' +  targetfile + '" -acodec mp3 -b 64k -f mp3 "' + \
+    cmd = 'avconv  -i "' +  targetfile + '" -b 64k -f mp3 "' + \
         outdir + '/' + name + '.mp3"'
-#    print cmd
     os.system(cmd)
     
-#avconv -i files/ProdigySmackMyBitchUp.wma -acodec mp2 -f mp2  out/file.mp3
-#avconv -i files/ProdigySmackMyBitchUp.wma -acodec libvorbis -f ogg -b 128k out/file.ogg
+#avconv -i in.wma -acodec mp2 -f mp2  out/file.mp3
+#avconv -i in.wma -acodec libvorbis -f ogg -b 128k out/file.ogg
 
