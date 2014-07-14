2fb8490e85914c5e12d73f74f429b3d48fb0a6b6
Daren Isaacs <ikkyisaacs@gmail.com>
2014-07-14 13:54:51 +1000

    New volume control script.

---
+++ pa-vol.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+from sys import argv
+import os
+
+inc = 0x800
+sink = "alsa_output.pci-0000_00_1b.0.analog-stereo"
+
+def getSetting(setting):
+    command = 'pacmd dump |grep ' + setting + '|grep ' + sink
+    resultList = os.popen(command).read()
+    return resultList .rsplit(" ")   
+
+def setSetting(setting, sink, value):
+    command = 'pacmd ' + setting + ' ' + sink + ' ' + value            
+    result = os.popen(command).read()
+
+if len(argv) == 2:
+    option = argv[1]
+    if option == 'plus':
+        result = getSetting('set-sink-volume')
+        volume = int(result[2], 16)
+        volume += inc
+        if volume > 0x10000:
+            volume = 0x10000
+        setSetting(result[0], result[1], "0x{0:x}".format(volume))
+    elif option == 'minus':
+        result = getSetting('set-sink-volume')
+        volume = int(result[2], 16)
+        volume -= inc
+        if volume < 0:
+            volume = 0x0
+        setSetting(result[0], result[1], "0x{0:x}".format(volume))
+    elif option == 'mute':
+        result = getSetting('set-sink-mute')
+        if result[2].rstrip() == 'yes':
+            value = 'no'
+        else: 
+            value = 'yes'
+        setSetting(result[0], result[1], value)
+else:
+    print "usage pa-vol.py plus | minus | mute"            
