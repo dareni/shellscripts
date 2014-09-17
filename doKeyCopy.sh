504e1e0b16eb00a9582bca78481f71041fa6923f
Daren Isaacs <ikkyisaacs@gmail.com>
2014-09-17 14:21:23 +1000

    Set the remote shell.

--- doKeyCopy.sh
+++ doKeyCopy.sh
@@ -2,5 +2,5 @@
 if [ -z "$1" ]; then
     echo "Usage: ./doKeyCopy.sh username@host.domain"
 else
-    cat ~/.ssh/id_rsa.pub | ssh $1 'dd >> ~/.ssh/authorized_keys; if [ ! -d ~/.ssh ]; then mkdir ~/.ssh; chmod 700 ~/.ssh; echo Just created ~/.ssh please run again; fi'
+    cat ~/.ssh/id_rsa.pub | ssh $1 'dd >> ~/.ssh/authorized_keys; /bin/sh -c "if [ ! -d ~/.ssh ]; then mkdir ~/.ssh; chmod 700 ~/.ssh; echo Just created ~/.ssh please run again; fi"'
 fi
