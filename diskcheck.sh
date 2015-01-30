4795a8ea2548dd7e429e7a1ecdee60f21ce9b58c
Daren Isaacs <ikkyisaacs@gmail.com>
2015-01-30 14:40:53 +1000

    New script for alert on disk usage threshold.

---
+++ diskcheck.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Send a mail to root when the disk usage passes a threshold.
+# Usage: diskcheck.sh [filesystem] [threshold%]
+
+if [ -z "$1" ]; then
+    FS="/"
+else
+    FS="$1"
+fi
+
+if [ -z "$2" ]; then
+    THRESHOLD=98
+else
+    THRESHOLD=$2
+fi
+
+CAPACITY=`df ${FS} | awk '{ if (NR == 2) print $5}' |sed 's/%//'`
+
+if [ ${CAPACITY} -ge ${THRESHOLD} ]; then
+   echo "`hostname` ${FS} file system usage ${CAPACITY}%" | mail -s "$FS file system usage ${CAPACITY}%" root@localhost
+fi
+
