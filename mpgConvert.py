2988f09494871e5ce985b5d5103c44e72d2a3cee
Daren Isaacs <ikkyisaacs@gmail.com>
2015-03-09 07:47:39 +1000

    Many bad bugs fixed in mpgConvert.py.

--- mpgConvert.py
+++ mpgConvert.py
@@ -4,18 +4,22 @@
 import os
 from sys import argv
 
-if len(argv) <> 3:
-    print 'Usage: mpgConvert.py infile/dir outdir [bitrate]'
+def displayError():
+    print 'Usage: mpgConvert.py [infile]|[indir] outdir [bitrate]'
     print 'Example:  mpgConvert.py ./song.mp3  /tmp 64k'
+    print 'Example:  mpgConvert.py /songs /tmp/songs 64k'
     print 'Default bitrate=128k'
-    exit
-    raise Exception('Incorrect usage.')
+
+if len(argv) < 3 or len(argv) > 4:
+    displayError()
+    raise Exception('Incorrect usage, please specify parameters.')
 
 target = argv[1]
 outdir = argv[2]
-bitrate = '128kb'
-if len(argv) == 3:
-    bitrate = argv[2]
+bitrate = '128k'
+print len(argv)
+if len(argv) == 4:
+    bitrate = argv[3]
     files = []
 if os.path.isfile(target):
     files = [target]
@@ -24,18 +28,20 @@ elif os.path.isdir(target):
     adjFiles = []
     for song in files:
         adjFiles.append(target + '/' + song)
-    files = adjFiles 
-    
+    files = adjFiles
+
 if not os.path.isdir(outdir):
-    os.system('mkdir ' + outdir)
+    displayError()
+    raise Exception('Outdir is not a directory.')
 
 for targetfile in files:
     filename = os.path.basename(targetfile)
     name, extension = os.path.splitext(filename)
     cmd = 'avconv  -i "' +  targetfile + '" -b "' + bitrate + '" -f mp3 "' + \
         outdir + '/' + name + '.mp3"'
+    print cmd
     os.system(cmd)
-    
+
 #avconv -i in.wma -acodec mp2 -f mp2  out/file.mp3
 #avconv -i in.wma -acodec libvorbis -f ogg -b 128k out/file.ogg
 
