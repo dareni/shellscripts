6bca7a5a511ba86d5fbbf7e4a647a363ae10e4a8
Daren Isaacs <ikkyisaacs@gmail.com>
2015-10-02 09:36:29 +1000

    New rcsCheck script.

---
+++ rcsCheck.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+locate RCS | grep ",v" | awk '{
+    segNum=split($0,segs,"/")
+    rcsfile=segs[segNum]
+    rcsfile=substr(rcsfile,1,length(rcsfile)-2)
+    filePathPOS=index($0,"RCS")
+    filePath=substr($0,1,filePathPOS-1)
+    absFile=filePath""rcsfile
+    print  $0" "absFile
+}' |xargs -n 2 rcsdiff 2>&1 |vim -R -
