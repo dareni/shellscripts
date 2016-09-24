4350674b6b08220787095976ffccf31f9708a97b
ewan <ewanisaacs18@gmail.com>
2016-09-24 12:40:16 +1000

    Custom fvwm for host ewan.

--- config/sweetpea.sh
+++ config/sweetpea.sh
@@ -24,17 +24,11 @@ do_init() {
     XLOAD_POS=$(($X-$XLOAD_POS))
     TRAY_POS=$(($X-$TRAY_POS))
 
-    xclock -digital -update 1 -norender -padding -5 -geometry 70x14+$TIME_POS+0 -strftime " %H:%M:%S" &
-    xclock -digital  -norender -padding -5 -geometry 115x14+$DATE_POS+0 -strftime  " %a %b %d %Y" &
+    xclock -digital -update 1 -norender -padding 0 -geometry 70x14+$TIME_POS+0 -strftime " %H:%M:%S" &
+    xclock -digital  -norender -padding 0 -geometry 115x14+$DATE_POS+0 -strftime  " %a %b %d %Y" &
     xload -geometry 100x28+$XLOAD_POS+0 -nolabel &
     stalonetray -i 16 --geometry 1x1+$TRAY_POS+0 --grow-gravity NE &
 
-#    xclock -digital -update 1 -norender -padding -5 -geometry 70x14+1735+0 -strftime " %H:%M:%S" &
-#    xclock -digital  -norender -padding -5 -geometry 115x14+1800+0 -strftime  " %a %b %d %Y" &
-#    xload -geometry 100x28+1620+0 -nolabel &
-#    stalonetray -i 16 --geometry 1x1+1570+0 --grow-gravity NE &
-#    xterm +sb -g 100x58+0+0 -fn 6x13 &
-#    xterm +sb -g 100x22+0+772 -fn 6x13 &
 }
 
 case $CMD in
