4eaf0611c2a3c4e09d707a12e383b48714c0c278
Daren Isaacs <ikkyisaacs@gmail.com>
2016-11-22 11:09:53 +1000

    New git patch script.
    Add istanbul to the office menu.

---
+++ gitpatch.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Maintained at: git@github.com:dareni/shellscripts.git
+echo "gitpatch.sh: \n
+1) stash all changes in the current workspace including new and deleted files, \n\
+2) create a patch of the stashed changes.
+"
+read -p "Continue (y/n): " CONTINUE
+
+if [ y != "$CONTINUE" -a Y != "$CONTINUE" ]; then
+  exit 0;
+fi
+if [ -d ".git" ]; then
+  git add -A
+  git stash save "Patch" 
+  DATE=`date +%y%m%d`
+  git stash show -p > git.patch.$DATE
+  echo "Now use: git apply git.patch"
+else 
+  echo "Must be a git repository."    
+fi
