c26b300fec7f7e3e9f55b7964fe019c9249aad6b
Daren Isaacs <ikkyisaacs@gmail.com>
2017-05-09 09:03:29 +1000

    Allow backup of renamed git files.

--- gitTarBup.sh
+++ gitTarBup.sh
@@ -1,18 +1,46 @@
 #!/bin/sh
 # Maintained at: git@github.com:dareni/shellscripts.git
 echo "gitTarBup.sh: \n
-Copy each modified file into a tar.
+From the current git directory copy each modified file into a tar.
 "
+
+git status --porcelain .
+
 read -p "Continue (y/n): " CONTINUE
+
 
 if [ y != "$CONTINUE" -a Y != "$CONTINUE" ]; then
   exit 0;
 fi
-if [ -d ".git" ]; then
-NOWDATE=`date +%Y%m%d%H%M`
-NAME=`basename \`pwd\``
-NAME=$NAME-${NOWDATE}.tar.gz
-  git status --porcelain |awk '{print $2}' | tar -cvzf $NAME --exclude=$NAME -T -
+STARTDIR=`pwd`
+GITDIR=$STARTDIR
+GITDIRVALID=0
+
+while [ $GITDIR != / -a $GITDIRVALID -eq 0 ]; do
+  if [ -d "$GITDIR/.git" ]; then
+    GITDIRVALID=1
+  else
+    GITDIR=`readlink -f ${GITDIR}/..`
+  fi
+done
+
+if [ $GITDIRVALID -eq 1 ]; then
+  NOWDATE=`date +%Y%m%d%H%M`
+  NAME=`basename \`pwd\``
+  NAME=$NAME-${NOWDATE}.tar.gz
+  git status --porcelain . | \
+    awk '{
+      if (NF==2) {
+         print "'$GITDIR/'"$2
+      } else if ($1="RM") {
+         print "'$GITDIR/'"$4
+      } else {
+         print "Did not process -> " $0 > "/dev/stderr"
+      }
+     }' | \
+  tar -cvzf "$GITDIR/$NAME" --exclude="$GITDIR/$NAME"  -T -
+  echo
+  echo $GITDIR/$NAME complete.
 else
   echo "Must be a git repository."
 fi
