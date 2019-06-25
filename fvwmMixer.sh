2aad75a61d6270acc2df9b057fb32a43ca415c3f
dareni <ikkyisaacs@gmail.com>
2019-06-25 10:35:22 +1000

    Pi fixes.

--- fvwmMixer.sh
+++ fvwmMixer.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 # Maintained at: git@github.com:dareni/shellscripts.git
 
-vol=`amixer get Master |tail -1 |awk '{print $4}'`
-if [[ "${1}" = "up" ]]; then
-    vol=$((vol+6500))
-    if [[ $vol -gt 65536 ]]; then
-        vol=65535
+CTRL=`amixer scontrols |awk '{print $4}'|awk -F"[']" '{ print $2}'`
+vol=`amixer get $CTRL |tail -1 |awk '{print $4}' | \
+  cut -c1 --complement |cut -f1 -d%`
+if [ "${1}" = "up" -o "${1}" = "plus" ]; then
+    vol=$((vol+5))
+    if [ $vol -gt 100 ]; then
+        vol=100
     fi 
 else
-    vol=$((vol-6500))
-    if [[ ( $vol -lt 0 ) || ( "${1}" = "mute" ) ]]; then
+    vol=$((vol-5))
+    if [ $vol -lt 0 -o "${1}" = "mute" ]; then
         vol=0
     fi 
 fi;
-amixer set Master $vol  > /dev/null
+amixer set $CTRL $vol%  > /dev/null
 
 
--- xbacklight.sh
+++ xbacklight.sh
@@ -2,22 +2,23 @@
 
 CMD=$1
 
-if [ -z "$CMD" ]; then
-   SETTING=`xbacklight -get `
-   SETTING=`printf %.0f $SETTING`
+if [ -x xbacklight ]; then
+  if [ -z "$CMD" ]; then
+    SETTING=`xbacklight -get `
+    SETTING=`printf %.0f $SETTING`
 
-   if [ -z "$SETTING" ]; then
-       xbacklight -set 50
-       echo 50
-   elif [ "$SETTING" -ge 90 ]; then
-       xbacklight -set 10
-       echo 10
-   else
-       SETTING=`echo "$SETTING+10" |bc|tail -1`
-       xbacklight -set $SETTING
-       echo set
-   fi
-else
+    if [ -z "$SETTING" ]; then
+      xbacklight -set 50
+      echo 50
+    elif [ "$SETTING" -ge 90 ]; then
+      xbacklight -set 10
+      echo 10
+    else
+      SETTING=`echo "$SETTING+10" |bc|tail -1`
+      xbacklight -set $SETTING
+      echo set
+    fi
+  else
     if [ ${CMD} = "plus" ]; then
         xbacklight -inc 10
         echo plus
@@ -25,4 +26,18 @@ else
         xbacklight -dec 10
         echo dec
     fi
+  fi
+elif [ -f /sys/class/backlight/rpi_backlight/brightness ]; then
+  #Raspberry Pi LCD backlight
+  LEVEL=`cat /sys/class/backlight/rpi_backlight/brightness`
+  if [ ${CMD} = "plus" ]; then
+    if [ $LEVEL -lt 90 ]; then
+      LEVEL=$(($LEVEL+5))
+    fi
+  else
+    if [ $LEVEL -gt 15 ]; then
+      LEVEL=$(($LEVEL-5))
+    fi
+  fi
+  echo $LEVEL | sudo tee /sys/class/backlight/rpi_backlight/brightness
 fi
