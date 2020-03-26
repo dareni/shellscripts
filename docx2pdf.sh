4b3e47896a8b71b6ba5b830aa71a1dd21dd9e44c
dareni <ikkyisaacs@gmail.com>
2020-03-26 17:41:55 +1000

    Allow converion of docx to pdf.

---
+++ docx2pdf.sh
@@ -0,0 +1,10 @@
+# Require pandoc pdflatex texlive-latex-recommended texlive-fonts-recommended
+FILE=$1
+FILENOPATH=${FILE##*/}
+FILENOEXT=${FILENOPATH%.*}
+
+if [[ -z "$FILE" ]]; then
+  echo Usage: docx2pdf filename.docx
+else
+  pandoc -o $FILENOEXT.pdf -f docx $FILE
+fi
