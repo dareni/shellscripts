c8a5fe17023d59d13f8d8ea4572499b488d63bf3
dareni <ikkyisaacs@gmail.com>
2020-03-27 12:09:58 +1000

    Bug fix epub builder.

--- makeEpub.sh
+++ makeEpub.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+LANG=C
 
 #Dependencies:pandoc2.9 texlive-fonts-recommended texlive-xetex
 VERSION=`pandoc -v |head -1|cut -d" " -f2`
@@ -9,7 +10,6 @@ if [ "$VERSION_MAJOR" -lt 2 ] ||  [ "$VERSION_MAJOR" -eq 2 -a  "$VERSION_MINOR" 
   echo Pandoc minimum version 2.9 required. Detected version: $VERSION
   exit
 fi
-echo ok;
 
 CMD=$1
 if [ -z "$CMD" ]; then
@@ -36,9 +36,12 @@ if [ $JPG_COUNT -ne $FILE_COUNT ]; then
   exit
 fi
 
-HR_TAG="<hr/>"
 PARA_OPEN="<p>"
+#PARA_CLOSE tag must be printed at the end of a line only
+#  because an image may be printed after it.
 PARA_CLOSE="</p>"
+IMG_TAG="<img"
+BREAK_TAG="<br/>"
 
 IMG_DIR=img
 mkdir -p $IMG_DIR
@@ -49,9 +52,34 @@ TXT_DIR=txt
 mkdir -p $TXT_DIR
 HTML_DIR=html
 mkdir -p $HTML_DIR
+EPUB_CSS="epub.css"
+
+makeEpubCss() {
+cat <<HEREDOC > $EPUB_CSS
+/* This defines styles and classes used in the book */
+body { margin: 5%; text-align: justify; font-size: medium; }
+code { font-family: monospace; }
+h1 { text-align: left; }
+h2 { text-align: left; }
+h3 { text-align: left; }
+h4 { text-align: left; }
+h5 { text-align: left; }
+h6 { text-align: left; }
+h1.title { }
+h2.author { }
+h3.date { }
+ol.toc { padding: 0; margin-left: 1em; }
+ol.toc li { list-style-type: none; margin: 0; padding: 0; }
+a.footnote-ref { vertical-align: super; }
+em, em em em, em em em em em { font-style: italic;}
+em em, em em em em { font-style: normal; }
+.aligncenter { text-align:center; }
+.small { font-size: small};
+HEREDOC
+}
 
 doCreateHtml() {
-  cat txt/$PGNO.txt | awk \
+  cat txt/$PGNO.txt |iconv -f utf-8 -t ascii//TRANSLIT | awk \
      'BEGIN{paragraph=0; blank=0; fullstop=0}
       {
         if ($0 !~ /[a-z]/) {
@@ -60,44 +88,58 @@ doCreateHtml() {
              #Must be all uppercase.
              if (NR == 1) {
                #All caps first line so treat as heading.
-               print "<h1>"$0"</h1><BR>"
+               print "<h1>"$0"</h1><br/>"
              } else {
                #All caps on a line so force a break.
-               print $0"<BR>"
+               print $0"<br/>"
                blank=0
              }
            } else {
-             #check for blankline
-             line=$0
-             gsub(/ /, "", line)
-             if (length(line) == 0) {
-               blank=1
-               if (paragraph == 1 && fullstop == 1) {
-                  print "'$PARA_CLOSE'"$0
-               }
-             } else {
-              line=$0
-              #look for eof etc and remove.
-              gsub(/[\x00-\x1F\x7F]/,"",line);
-              if (length(line) == 0) {
-                #last line - do nothing
-              } else {
-                #Not a blank must be text.
-                if (blank == 1) {
-                  print "'$PARA_OPEN'"$0
-                  blank = 0
-                  paragraph = 1
-                  endChar = substr($0,length($0),1)
-                  if (endChar == ".") {
-                    fullstop=1
-                  }
-                }
+            #No a-z no A-Zupper.
+            #remove spaces and control chars
+            gsub(/[ \x00-\x1F\x7F]/,"",line);
+            #check for blankline
+            if (length(line) == 0) {
+              blank=1
+              if (paragraph == 1 && fullstop == 1) {
+                 print "'$PARA_CLOSE'"
+                 paragraph = 0
+                 fullstop = 0
               }
              }
            }
         } else {
           #Line has lower case chars.
-          print $0
+          #Not a blank must be text.
+          startChar = substr($0,1,1)
+          if (NR == 1) {
+            if (startChar  ~ /[\x22\x27\x40A-Z]/) {
+              print "'$PARA_OPEN'"$0
+              paragraph = 1
+            }  else if (startChar  ~ /[a-z]/) {
+              paragraph = 1
+              print $0
+            } else {
+              print $0
+            }
+          }else if (blank == 1) {
+            if (paragraph == 0 && (startChar  ~ /[\x22\x27\x40A-Z]/)) {
+              print "'$PARA_OPEN'"$0
+              paragraph = 1
+            } else {
+              print $0
+            }
+            blank = 0
+          } else {
+            print $0
+          }
+          endChar = substr($0,length($0),1)
+          #treat fullstop as . or ? or single or double quote.
+          if (endChar  ~ /[\x2e\x3f\x22\x27\x40]/) {
+            fullstop=1
+          } else {
+            fullstop=0
+          }
         }
       }
       END {
@@ -108,21 +150,25 @@ doCreateHtml() {
      ' >  $HTML_OP_FILE
 
 }
-
-
-if [ "$CMD" = "img" ]; then
-
+if [ "$CMD" = "test" ]; then
+  PGNO=$2
+  HTML_OP_FILE=$3
+  doCreateHtml
+  echo output1: $HTML_OP_FILE
+  exit
+elif [ "$CMD" = "img" ]; then
 
   echo Click top left image corner ...
   WINID=`xdotool selectwindow`
   MOUSELOC=`xdotool getMouselocation`
-  MOUSEX1=`echo $F_MOUSELOC|cut -d" " -f1|cut -d: -f2`
-  MOUSEY1=`echo $F_MOUSELOC|cut -d" " -f2|cut -d: -f2`
+  MOUSEX1=`echo $MOUSELOC|cut -d" " -f1|cut -d: -f2`
+  MOUSEY1=`echo $MOUSELOC|cut -d" " -f2|cut -d: -f2`
   echo Click bottom right image corner ...
+
   WINID=`xdotool selectwindow`
   MOUSELOC=`xdotool getMouselocation`
-  MOUSEX2=`echo $F_MOUSELOC|cut -d" " -f1|cut -d: -f2`
-  MOUSEY2=`echo $F_MOUSELOC|cut -d" " -f2|cut -d: -f2`
+  MOUSEX2=`echo $MOUSELOC|cut -d" " -f1|cut -d: -f2`
+  MOUSEY2=`echo $MOUSELOC|cut -d" " -f2|cut -d: -f2`
 
   WIDTH=$(($MOUSEX2-$MOUSEX1))
   HEIGHT=$(($MOUSEY2-$MOUSEY1))
@@ -141,32 +187,21 @@ if [ "$CMD" = "img" ]; then
     if [ -n "$IMG_TXT" ]; then
       IMG_TXT="<italic>$IMG_TXT</italic>"
     fi
-    IMAGE_TAG='<img src="'$IMG_FILENAME'"><br>'${IMG_TXT}${HR_TAG}
+    IMAGE_TAG_COMPLETE=${IMG_TAG}' src="'$IMG_FILENAME'">'${BR_TAG}${IMG_TXT}
 
     HTML_OP_FILE="html/$PGNO.html"
     HTML_OP_FILE_MOD="$MOD_DIR/${PGNO}.html"
 
     if [ ! -e $HTML_OP_FILE_MOD ]; then
       if [ ! -e $HTML_OP_FILE ]; then
-        echo ${HR_TAG} > $HTML_OP_FILE_MOD
+        touch $HTML_OP_FILE_MOD
       else
         cp $HTML_OP_FILE $HTML_OP_FILE_MOD
       fi
     fi
 
-    cat $HTML_OP_FILE_MOD | awk \
-       'BEGIN{}
-        {
-          if ($0 !~ '${HR_TAG}'/) {
-               line=$0
-               gsub("'${HR_TAG}'","",line);
-               print line"'$IMAGE_TAG'"
-             }
-          } else {
-            print $0
-          }
-        }
-       ' >  $HTML_OP_FILE_MOD
+    # Append the image to the file.
+    echo $IMAGE_TAG_COMPLETE >> $HTML_OP_FILE_MOD
 
 elif [ "$CMD" = "epub" ]; then
 
@@ -182,7 +217,11 @@ elif [ "$CMD" = "epub" ]; then
     echo "---\ntitle: '$B_TITLE'\nauthor:\n- '$B_AUTHOR'\nlanguage: en-US\n..." > $METADATA
   fi
 
-  PANDOC_CMD="pandoc -f html -t epub2 --css=./epub.css \
+  if [ ! -e ./$EPUB_CSS ]; then
+    makeEpubCss
+  fi
+
+  PANDOC_CMD="pandoc -f html -t epub2 --css=./$EPUB_CSS \
   -o book.epub  --epub-cover-image=$FRONT_COVER --metadata-file=$METADATA"
 
   for FILE in `ls -v *.jpg`
