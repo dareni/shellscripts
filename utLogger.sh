ffefcb7f8d817a41a046382841a0804d73c5dea2
dareni <ikkyisaacs@gmail.com>
2020-05-23 20:52:44 +1000

    New ut console logger.

---
+++ utLogger.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+DAY=`date +'%y%m%d'`
+LOGDIR=/var/log/ut
+mkdir -p $LOGDIR
+OUT=$LOGDIR/$DAY.log
+
+awk '{
+  if (// || //){
+  } else if (/\^1/ || /\^4/) {
+    if ($0 !~ /\^7/ ) {
+      print $0
+      fflush()
+    }
+  } else if (/\console:/ && (/Attack/ || /attack/ || /Upcoming/)) {
+      print $0
+      fflush()
+  } else if (/connected/ || /joined/ || /overflow/ || /\^5[auth]/) {
+      print $0
+      fflush()
+  }
+
+}' >> $OUT /dev/stdin
