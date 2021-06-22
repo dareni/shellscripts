957e6ca798e755b460a4c7c2e009bd24dc444129
dareni <ikkyisaacs@gmail.com>
2021-06-22 18:09:13 +1000

    New rsync pull. Bug fixes.

--- pull.sh
+++ pull.sh
@@ -2,15 +2,16 @@
 # Maintained at: git@github.com:dareni/shellscripts.git
 
 if [[ -z $1 ]]; then
-   echo No target directory. eg daren@192.168.1.4:/opt/film
+   echo No source directory. eg daren@192.168.1.4:/opt/film
    exit
 fi
+
 if [[ ! -e ./files ]]; then
   echo \'files\' containing a list of files for transfer does not exist.
   exit
 fi
 
-rsync -abHr --progress --no-implied-dirs --files-from=./files -e ssh . ${1}
+rsync -abHr --progress --no-implied-dirs --files-from=./files ${1} .
 
 #files example:
 #/opt/files/file.txt
--- vidStream.sh
+++ vidStream.sh
@@ -5,7 +5,7 @@ if [ -z "$1" ]; then
   echo client:  vidStream.sh hostip
 elif [ -z "$2" ]; then
   #nc 192.168.1.11 5500 |mplayer -cache 8192 -
-  nc $1 5500 |mplayer -cache 8192 -
+  nc $1 5500 |mpv -
 else
   echo server listening for connection ....
   #nc -l -p 5500 192.168.1.11 < v.mp4.crdownload
