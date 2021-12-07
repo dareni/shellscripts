0c7ac720e99abcd37b99a83e8518b9951a9328cd
dareni <ikkyisaacs@gmail.com>
2021-12-08 07:50:56 +1000

    Bugfix funcPower.sh

--- config/funcPower.sh
+++ config/funcPower.sh
@@ -1,37 +1,37 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 TEST=$1
 
 POWER_CMD="upower -d"
 
-function getStats() {
+getStats() {
   $POWER_CMD |egrep 'on-battery|percentage'
 }
 
-function showMessage() {
+showMessage() {
   wall $1
   (xterm -geometry 100x6-0+0 -fa *courier* -fs 12 -bg darkgray -fg black -e ~/bin/shellscripts/timer.sh 0 "$1")&
 }
 
-function getMessage() {
+getMessage() {
   PERCENT=$1
   ONBAT=$2
   OLD_PERCENT=$3
   MSG=""
-  if [[ -z "$PERCENT" ]]; then
+  if [ -z "$PERCENT" ]; then
     MSG="Error: PERCENT value is invalid:$PERCENT"
   elif [[ ! $PERCENT =~ ^-?[0-9]+$ ]]; then
     MSG="Error calculating battery capacity."
-  elif [[ "$ONBAT" = yes ]]; then
-   if [[ -z "$OLD_PERCENT" ]]; then
-     if [[ $PERCENT -eq 100 ]]; then
+  elif [ "$ONBAT" = yes ]; then
+   if [ -z "$OLD_PERCENT" ]; then
+     if [ $PERCENT -eq 100 ]; then
        MSG="On battery 100% capacity."
         OLD_PERCENT=90
      else
        MSG="On battery $PERCENT capacity."
        OLD_PERCENT=$(($PERCENT / 10 * 10))
      fi;
-    elif [[ $PERCENT -lt $OLD_PERCENT ]]; then
+    elif [ $PERCENT -lt $OLD_PERCENT ]; then
        OLD_PERCENT=$((${PERCENT} / 10 * 10))
        MSG="On battery. Down to $PERCENT% capacity."
     fi
@@ -42,19 +42,19 @@ function getMessage() {
   echo $MSG
 }
 
-function decodeMessage() {
+decodeMessage() {
   MSG=$1
   TXT=`echo "$MSG" | cut -d, -f 1`
   echo $TXT
 }
 
-function decodePercent() {
+decodePercent() {
   MSG=$1
   OLD_PERCENT=`echo "$MSG" | cut -d, -f 2-`
   echo $OLD_PERCENT
 }
 
-function testIt() {
+testIt() {
   RET=`decodeMessage "1 2, 3"`
   assert "decodeMessage test" "1 2" "$RET"
 
@@ -87,17 +87,17 @@ function testIt() {
   assert "mParse1" "Error: PERCENT value is invalid:" "$TXT"
 }
 
-function assert() {
+assert() {
   MSG=$1
   ARG1=$2
   ARG2=$3
-  if [[ "$ARG1" != "$ARG2" ]]; then
+  if [ "$ARG1" != "$ARG2" ]; then
     echo error: $MSG $ARG1 != $ARG2
     return 1;
   fi
 }
 
-function getStatus() {
+getStatus() {
   STAT=`getStats`
   PERCENT=`echo $STAT | awk '{print $4}'| awk -F% '{print $1}'`
   ONBAT=`echo $STAT | awk '{print $6}'`
@@ -110,20 +110,23 @@ function getStatus() {
 }
 
 
-if [[ "$TEST" = TEST ]]; then
+if [ -n "$TEST" ]; then
+  echo "Testing.."
   testIt
 else
   EPID=`pgrep -o funcPower.sh`
+  if [ -n "$EPID" ]; then
     if [ "$$" -ne "$EPID" ]; then
       kill -9 $EPID
     fi
-    OLD_PERCENT="";
-    (while [ 1 ]; do
-      TXT=`getStatus`
-      if [[ -n $TXT ]]; then
-        showMessage "$TXT"
-      fi
-      sleep 60
-    done)&
+  fi
+  OLD_PERCENT="";
+  (while [ 1 ]; do
+    TXT=`getStatus`
+    if [ -n "$TXT" ]; then
+      showMessage "$TXT"
+    fi
+    sleep 60
+  done)&
 fi
 
