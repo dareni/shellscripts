5358d8d1040872b4a6076e87eabee6a8b8ed4aa2
Daren Isaacs <ikkyisaacs@gmail.com>
2022-07-06 18:15:06 +1000

    Laptop config adjustments.

--- synclient.sh
+++ synclient.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env sh
 # Maintained at: git@github.com:dareni/shellscripts.git
-PAD=`synclient |grep TouchpadOff |awk '{print $3}'`
-if [ $PAD -eq 0 ]; then
-  synclient TouchpadOff=1;
-else
-  synclient TouchpadOff=0;
-fi
 
+#ON or OFF, empty is a toggle.
+TOUCH_PAD=$1
+if [ -n "`which synclient`" ]; then
+  if [ -z "$TOUCH_PAD" ]; then
+    PAD=`synclient |grep TouchpadOff |awk '{print $3}'`
+  else
+    if [ "ON" = "$TOUCH_PAD" ]; then
+      PAD=1
+    else
+      PAD=0
+    fi
+  fi
+  if [ $PAD -eq 0 ]; then
+    synclient TouchpadOff=1;
+  else
+    synclient TouchpadOff=0;
+  fi
+fi
