27362709e23ac5d2b6c6ccad546ac229410ef7a9
Daren Isaacs <ikkyisaacs@gmail.com>
2024-11-08 06:55:38 +1000

    Adjust pa-vol.py for pipewire.
    Use pactl instead of pacmd, because it does not exist with the new
    pipewire config.

--- pa-vol.py
+++ pa-vol.py
@@ -2,43 +2,51 @@
 # Maintained at: git@github.com:dareni/shellscripts.git
 from sys import argv
 import os
+import logging
 
+#logging.basicConfig(level=logging.DEBUG)
 inc = 0x800
 #sink = "alsa_output.pci-0000_00_1b.0.analog-stereo"
 #sink = "alsa_output.pci-0000_00_1b.0"
-sink = os.popen('pacmd dump|grep set-default-sink').read().split(" ")[1]
+#sink = os.popen('pacmd dump|grep set-default-sink').read().split(" ")[1]
+pacmd = 'pactl'
 
-def getSetting(setting):
-    command = 'pacmd dump |grep ' + setting + '|grep ' + sink
+def getSetting(sink, cmd):
+    command = pacmd + ' ' + cmd + ' ' + sink
+    logging.debug('command::::' + command)
     resultList = os.popen(command).read()
-    return resultList .rsplit(" ")   
+    logging.debug("result::::: " + resultList)
+    return resultList .rsplit(" ")
 
-def setSetting(setting, sink, value):
-    command = 'pacmd ' + setting + ' ' + sink + ' ' + value            
+def setSetting(sink, cmd,  value):
+    command = pacmd + ' ' + cmd + ' ' + sink + ' ' + str(value)
+    logging.debug('command::::' + command)
     result = os.popen(command).read()
 
 if len(argv) == 2:
+    sink = os.popen(pacmd + ' get-default-sink').read().strip()
     option = argv[1]
     if option == 'plus':
-        result = getSetting('set-sink-volume')
-        volume = int(result[2], 16)
+        result = getSetting(sink, 'get-sink-volume')
+        volume = int(result[2], 10)
+        logging.debug("volume::::: " + str(volume))
         volume += inc
         if volume > 0x10000:
             volume = 0x10000
-        setSetting(result[0], result[1], "0x{0:x}".format(volume))
+        setSetting(sink, 'set-sink-volume', volume)
     elif option == 'minus':
-        result = getSetting('set-sink-volume')
-        volume = int(result[2], 16)
+        result = getSetting(sink, 'get-sink-volume')
+        volume = int(result[2], 10)
         volume -= inc
         if volume < 0:
             volume = 0x0
-        setSetting(result[0], result[1], "0x{0:x}".format(volume))
+        setSetting(sink, 'set-sink-volume', volume)
     elif option == 'mute':
-        result = getSetting('set-sink-mute')
-        if result[2].rstrip() == 'yes':
+        result = getSetting(sink, 'get-sink-mute')
+        if result[1].rstrip() == 'yes':
             value = '0'
-        else: 
+        else:
             value = '1'
-        setSetting(result[0], result[1], value)
+        setSetting(sink, 'set-sink-mute', value)
 else:
     print("usage pa-vol.py plus | minus | mute")
