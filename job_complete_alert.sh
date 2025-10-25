71ea8259e919e2ef245058ad312aa8312039e4e6
Daren Isaacs <ikkyisaacs@gmail.com>
2025-10-25 14:35:53 +1000

    fix process monitor script

--- job_complete_alert.sh
+++ job_complete_alert.sh
@@ -2,6 +2,7 @@
 #Beep when the processor usage drops to zero.
 
 PID="$1"
+LEVEL="$2"
 
 # Check if a process name is provided as an argument
 if [ -z "$PID" ]; then
@@ -12,9 +13,10 @@ fi
 # Monitor the CPU usage
 while true; do
   # Get CPU usage
-  CPU_USAGE=$(ps --cumulative -h -o pcpu -p $PID | tr -d '[:space:]')
-  BC=$(ps -h -o pcpu --ppid $PID | tr '\n' '+' | tr -d '[:space:]' | sed 's/.$//')
-  CPU_USAGE=$(echo ${BC:-0}"+"${CPU_USAGE:-0} | bc -l)
+  PID_LIST=$(pstree $PID -p | grep -o "([0-9]*)" | tr -d '()' | tr '\n' ' ')
+  CPU_USAGE=$(ps -h -o pcpu -p $PID_LIST | tr '\n' '+' | tr -d '[:space:]' | sed 's/.$//')
+  CPU_USAGE=$(echo $CPU_USAGE | bc -l)
+
   # Check if the process is running
   if [ -z "$CPU_USAGE" ]; then
     echo pid $PID complete!
@@ -22,7 +24,7 @@ while true; do
   elif [ $(echo $CPU_USAGE | wc -w) -gt 1 ]; then
     echo Too many processes? Did you enter the parent pid?
     break
-  elif [[ 1 -eq $(echo "$CPU_USAGE==0" | bc -l) ]]; then
+  elif [[ 1 -eq $(echo "$CPU_USAGE<=${LEVEL:-0}" | bc -l) ]]; then
     # No cpu usage
     echo pid $PID %Usage = $CPU_USAGE
     beep
