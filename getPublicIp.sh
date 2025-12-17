9c55350cc8f3365d8986ec6f4832ad955c668c78
Daren Isaacs <ikkyisaacs@gmail.com>
2025-12-17 21:01:14 +1000

    feat: hack getPublicIp.sh for new router

--- getPublicIp.sh
+++ getPublicIp.sh
@@ -1,17 +1,17 @@
+#!/bin/env bash
 # Pull the public ip address from the firewall.
 # Maintained at: git@github.com:dareni/shellscripts.git
+
+if [[ -z "$2" ]]; then
+  wget 192.168.1.253 -O - | grep 'WAN IP' | awk -F'<strong>|</strong></font>' '{print $2}'
+  exit
+fi
 
 NAME=$1
 PASSWORD=$2
 
-if [[ -z "$2" ]]; then
-    echo Usage getPublicIp.sh [name] [password]
-    exit;
-fi
-
-
 #Retrieve the post action url.
-URL=`wget 192.168.1.253 -O - | grep '<form' |awk '{print $3}'| sed 's/^........//' | sed 's/.$//'`
+URL=$(wget 192.168.1.253 -O - | grep '<form' | awk '{print $3}' | sed 's/^........//' | sed 's/.$//')
 #Post the username/password to access the status page containing the public ip.
-wget -O - --post-data 'HngLoginUserName='${NAME}'&HngLoginPassword='${PASSWORD} 192.168.1.253${URL} \
-         | grep '^<div.*WAN IP' |sed 's/^.\{60\}//' |sed 's/..strong.*$//'
+wget -O - --post-data 'HngLoginUserName='${NAME}'&HngLoginPassword='${PASSWORD} 192.168.1.253${URL} |
+  grep '^<div.*WAN IP' | sed 's/^.\{60\}//' | sed 's/..strong.*$//'
